dff679ea07a45db59f6c2bd647222373fb0241e8
Abdou <kirakira1614@gmail.com>
2022-09-17 23:04:11 +0200

    'sugnup'

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('signup')
+    async createUser(@Body() input: CreateUserDto) {
+        return this.authService.signup(input)
+    }
+}
