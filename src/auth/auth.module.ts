c59ff796a3f1ddab045440821a7fde4a5fded396
Abdou <kirakira1614@gmail.com>
2022-09-17 22:58:43 +0200

    'signup'

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
-  imports: [PrismaService]
+  imports: [PrismaModule]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user';
+import {hash} from 'bcrypt'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private prisma: PrismaService) {}
+
+    async signup(input: CreateUserDto) {
+        const hashed = await hash(input.password, 12)
+        try{
+            const user = await this.prisma.user.create({
+                data: {
+                    email: input.email,
+                    hash: hashed
+                }
+            })
+            return user
+        }catch(e) {
+            if(e instanceof PrismaClientKnownRequestError) {
+                if(e.code === 'P2002') {
+                    throw new ForbiddenException('Email Already Taken')
+                }
+            }
+        }
+    }
+
+    async login() {}
+
+    async logout() {}
+
+    async refreshtoken() {}
+}
